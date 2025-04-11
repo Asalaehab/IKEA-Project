0b1321e74a49dfe76dab7b07d1f9a565ca57d03f
asalaehab824@gmail.com <asalaehab824@gmail.com>
2025-04-11 12:38:15 +0200

    auto-Mapper

--- IKEA.BLL/Profiles/MappingProfile.cs
+++ IKEA.BLL/Profiles/MappingProfile.cs
@@ -18,18 +18,23 @@ namespace IKEA.BLL.Profiles
             CreateMap<Employee, EmployeeDto>()
                 .ForMember(dest => dest.gender, Options => Options.MapFrom(src => src.gender))
                 .ForMember(dest => dest.EmpType, Options => Options.MapFrom(src => src.gender));
+
+
             CreateMap<Employee, EmployeeDetailsDto>()
                  .ForMember(dest => dest.gender, Options => Options.MapFrom(src => src.gender))
                 .ForMember(dest => dest.EmployeeType, Options => Options.MapFrom(src => src.gender))
                 .ForMember(dest => dest.HiringDate, Options => Options.MapFrom(src => DateOnly.FromDateTime(src.HiringDate)));
 
             CreateMap<CreatedEmployeeDto, Employee>()
+                .ForMember(dest => dest.gender, Options => Options.MapFrom(src => src.gender))
+                .ForMember(dest => dest.EmployeeType, Options => Options.MapFrom(src => src.gender))
                 .ForMember(dest => dest.HiringDate, options => options.MapFrom(src => src.HiringDate.ToDateTime(TimeOnly.MinValue)));
                         
             
-            CreateMap<UpdatedEmployeeDto, Employee>();
+            CreateMap<UpdatedEmployeeDto, Employee>()
+                 .ForMember(dest => dest.gender, Options => Options.MapFrom(src => src.gender))
+                .ForMember(dest => dest.EmployeeType, Options => Options.MapFrom(src => src.gender))
+                .ForMember(dest => dest.HiringDate, options => options.MapFrom(src => src.HiringDate.ToDateTime(TimeOnly.MinValue)));
         }
-
-
     }
 }
