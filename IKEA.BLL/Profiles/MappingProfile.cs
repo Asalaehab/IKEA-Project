8322870f16b2f94c3e917bc7ea54f33093d17432
asalaehab824@gmail.com <asalaehab824@gmail.com>
2025-04-14 22:35:16 +0200

    Some Changes in Edit view

--- IKEA.BLL/Profiles/MappingProfile.cs
+++ IKEA.BLL/Profiles/MappingProfile.cs
@@ -36,9 +36,10 @@ namespace IKEA.BLL.Profiles
 
 
             CreateMap<UpdatedEmployeeDto, Employee>()
-                 .ForMember(dest => dest.gender, Options => Options.MapFrom(src => src.gender))
-                .ForMember(dest => dest.EmployeeType, Options => Options.MapFrom(src => src.gender))
+             .ForMember(dest => dest.gender, Options => Options.MapFrom(src => Enum.Parse<Gender>(src.gender)))
+                .ForMember(dest => dest.EmployeeType, Options => Options.MapFrom(src => Enum.Parse<EmployeeType>(src.EmployeeType)))
                 .ForMember(dest => dest.HiringDate, options => options.MapFrom(src => src.HiringDate.ToDateTime(TimeOnly.MinValue)));
+
         }
     }
 }
